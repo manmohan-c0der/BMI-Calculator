e0bb3db2ccf4c7f0433545226a48a2b5194d74ad
Manmohan Sharma <129840143+manmohan-c0der@users.noreply.github.com>
2024-02-16 22:36:12 +0530

    BMI Calculator by using React.js
    BMI Calculator by using React.js

---
+++ App.jsx
@@ -0,0 +1,74 @@
+import './App.css'
+import './index.css'
+import React, {useState} from 'react'
+ 
+function App() {
+ 
+  // state
+  const [weight, setWeight] = useState(0)
+  const [height, setHeight] = useState(0)
+  const [bmi, setBmi] = useState('')
+  const [message, setMessage] = useState('')
+  const [error,setError]=useState(' ');
+ 
+  let calcBmi = (event) => {
+    //prevent submitting to the server
+    event.preventDefault()
+ 
+    if (weight === 0 || height === 0) {
+      // alert('Please enter a valid weight and height')
+      setError("Please enter a valid weight and height")
+    } else {
+      let bmi = (weight / (height * height) * 703)
+      setBmi(bmi.toFixed(1))
+ 
+      // Logic for message
+ 
+      if (bmi < 25) {
+        setMessage('You are underweight')
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage('You are a healthy weight')
+      } else {
+        setMessage('You are overweight')
+      }
+    }
+  }
+ 
+ 
+  let reload = () => {
+    window.location.reload()
+  }
+ 
+  return (
+    <div className="app">
+      <h1>{error}</h1>
+    <div className='container'>
+      <h2 className='center'>BMI Calculator</h2>
+      <form onSubmit={calcBmi}>
+ 
+        <div>
+          <label>Weight (lbs)</label>
+          <input value={weight} onChange={(e) => setWeight(e.target.value,setError(" "))} />
+        </div>
+ 
+        <div>
+          <label>Height (in)</label>
+          <input value={height} onChange={(event) => setHeight(event.target.value,setError(" "))} />
+        </div>
+ 
+        <div>
+          <button className='btn' type='submit'>Submit</button>
+          <button className='btn btn-outline' onClick={reload} type='submit'>Reload</button>
+        </div>
+      </form>
+ 
+      <div className='center'>
+        <h3>Your BMI is: {bmi}</h3>
+        <p>{message}</p>
+      </div>
+    </div>
+  </div>
+  );
+}
+ 
+export default App;
